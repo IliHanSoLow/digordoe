6691e5670b65f958b92e95bb41b4e7d39cfa2097
Ilian <ilian-ode@gmx.de>
2024-01-04 15:18:34 +0100

    Init commit

---
+++ src/main/java/org/io/digordoe/DigOrDoe.java
@@ -0,0 +1,17 @@
+package org.io.digordoe;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DigOrDoe extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
