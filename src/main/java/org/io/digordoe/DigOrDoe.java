1943ea5096ab22ee2d7a7bf515c3bfff18885ebc
Ilian <ilian-ode@gmx.de>
2024-01-07 17:37:53 +0100

    some changes

--- src/main/java/org/io/digordoe/DigOrDoe.java
+++ src/main/java/org/io/digordoe/DigOrDoe.java
@@ -1,13 +1,26 @@
 package org.io.digordoe;
 
+import com.onarandombox.MultiverseCore.MultiverseCore;
+import com.onarandombox.MultiverseCore.api.MultiverseWorld;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
+import java.util.List;
 
 public final class DigOrDoe extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
 
+        while (true){
+            MultiverseWorld world0 = core.getMVWorldManager().getMVWorld("world");
+            MultiverseWorld world1 = core.getMVWorldManager().getMVWorld("world_the_nether");
+            MultiverseWorld world2 = core.getMVWorldManager().getMVWorld("world_the_end");
+        }
     }
 
     @Override
